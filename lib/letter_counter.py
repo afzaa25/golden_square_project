da4e4661db9e2261d67f9d59d4bcda749bfdbbd0
Afzaa Atcha <afzaaatcha25@gmail.com>
2023-05-02 09:40:06 +0100

    updated projects

--- lib/letter_counter.py
+++ lib/letter_counter.py
@@ -9,12 +9,15 @@ class LetterCounter:
         for char in self.text: #starts a loop that goes over every letter in the text
             if not char.isalpha(): #checks if the letter is not from the alphabet
                 continue #if the character is not alphabetic, skips to the next iteration of the loop.
-            counter[char] = counter.get(char, 1) + 1#increments the count of the character in counter by 1, or initializes it to 1 if it does not yet exist in counter.
-            if counter[char] > most_common_count:#checks if the count of the character in counter is greater than the current most_common_count.
+            counter[char] = counter.get(char, 0) + 1 #increments the count of the character in counter by 1, or initializes it to 1 if it does not yet exist in counter.
+            if counter[char] > most_common_count: #checks if the count of the character in counter is greater than the current most_common_count.
                 most_common = char #if the count is greater than the current most_common_count, sets most_common to the current character.
-                most_common_count += counter[char]#if the count is greater than the current most_common_count, sets most_common_count to the new count.
+                most_common_count = counter[char] #if the count is greater than the current most_common_count, sets most_common_count to the new count.
         return [most_common_count, most_common]# returns a list containing the count of the most common letter in self.text and the most common letter itself.
 
 
-counter = LetterCounter("Digital Punk")
+counter = LetterCounter("Digital Punk") 
 print(counter.calculate_most_common())
+
+#current output = 3, D
+#actual output = 2, i
--- lib/vowel_remover.py
+++ lib/vowel_remover.py
@@ -7,8 +7,11 @@ class VowelRemover:
         i = 0 #initializes counter to 0
         while i < len(self.text): #this starts a loop that runs as long i is less than length of text
             if self.text[i].lower() in self.vowels: #this checks if the text at index i when coverted lowercase  is in vowels
-                self.text = self.text[:i] + self.text[i+1:] #if the character is a vowel, removes the vowel from self.text by concatenating the substring before and after the vowel at index i.
-            i += 1 #ncrements the counter variable i by one to move on to the next character in self.text.
+                self.text = self.text[:i] + self.text[i+1:]
+            else: #if the character is a vowel, removes the vowel from self.text by concatenating the substring before and after the vowel at index i.
+                i += 1 #ncrements the counter variable i by one to move on to the next character in self.text.
         return self.text # returns the modified string with all vowels removed.
 
-
+example_str = VowelRemover("aeiou")
+result_str = example_str.remove_vowels()
+print(result_str)
